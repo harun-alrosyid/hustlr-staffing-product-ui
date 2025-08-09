eeb85d5f4b7baf842a3a8eb864040b20772698c9
Harun Alrosyid <38546311+harun-alrosyid@users.noreply.github.com>
2025-08-09 17:12:37 +0000

    feat: config vite sandbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import path from "path";
+import { defineConfig } from "vite";
 
-import react from '@vitejs/plugin-react-swc';
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
+    strictPort: true,
+    allowedHosts: [/\.csb\.app$/, /\.codesandbox\.io$/],
+    hmr: {
+      protocol: "wss",
+      host: "https://mjqlf9-8080.csb.app",
+      clientPort: 443,
+    },
+    origin: "https://mjqlf9-8080.csb.app",
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
